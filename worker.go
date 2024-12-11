dd2dd473a2eae1af3ad4d4c3654de09eaf5b7fee
hongshengjie <hongshengjie@live.com>
2024-12-11 22:42:39 +0800

    make lib

---
+++ worker.go
@@ -0,0 +1,49 @@
+package meidsoupgo
+
+import (
+	"os"
+)
+
+type Channel struct{}
+
+type WebRtcServer struct{}
+
+type Router struct{}
+
+type (
+	WorkerObserverEvents struct{}
+	WorkerObserver       func(*WorkerObserverEvents)
+	WorkerAppData        struct{}
+)
+
+type Worker struct {
+	// mediasoup-worker child process.
+	child *os.Process
+
+	// Worker process PID.
+	pid int
+
+	// Channel instance.
+	channel *Channel
+
+	// Closed flag.
+	closed bool
+
+	// Died dlag.
+	died bool
+
+	// Worker subprocess closed flag.
+	subprocessClosed bool
+
+	// Custom app data.
+	appData WorkerAppData
+
+	// WebRtcServers set.
+	webRtcServers map[*WebRtcServer]struct{}
+
+	// Routers set.
+	routers map[*Router]struct{}
+
+	// Observer instance.
+	observer WorkerObserver
+}
